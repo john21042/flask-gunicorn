1dc80500755d3c2e71fa4f27e382c742c5592380
John <john@azorp.fios-router.home>
2022-08-15 23:19:12 -0400

    added logging

--- app/main.py
+++ app/main.py
@@ -1,9 +1,16 @@
 
 from flask import Flask
+from datetime import datetime
+import logging
+
 app = Flask(__name__)
 
-
+logging.basicConfig(level=logging.DEBUG, format=f'%(asctime)s %(levelname)s %(name)s %(threadName)s : %(message)s')
+ 
 @app.route('/')
 @app.route('/index')
 def index():
-    return 'Hello world!'
+    app.logger.info('/ called')
+    date_str =  datetime.now().strftime('%Y-%m-%d %I:%M:%S %p %Z')
+    # return 'Hello world! ' + datetime.datetime.now().strftime('%Y-%m-%d %I:%M:%S %p %Z')
+    return 'Hello world! ' + date_str
